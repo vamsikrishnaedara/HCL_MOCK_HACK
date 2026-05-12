97291949fa22974eb2a16992f313977cdbceaba9
jayanth-chepuri <jayanthnagagandhi_chepuri@srmap.edu.in>
2026-05-12 11:37:44 +0530

    first commit

---
+++ src/main/java/com/example/HCL_demo/HclDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.HCL_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclDemoApplication.class, args);
+	}
+
+}
